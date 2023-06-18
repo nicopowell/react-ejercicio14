e3aed36899588cbde63c5a7080f32809cbb96177
Nicolas Powell <nicopowell30@gmail.com>
2023-06-18 19:07:56 -0300

    le agregue unos margenes al container

--- src/components/views/Administrador.jsx
+++ src/components/views/Administrador.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import ItemReceta from "./recetas/ItemReceta";
 
 const Administrador = () => {
-
-    return (
-        <section className="container mainSection">
+  return (
+    <div className="container mainSection my-4">
+      <section>
         <div className="d-flex justify-content-between align-items-center mt-5">
           <h1 className="display-4 ">Recetas disponibles</h1>
-          <Link className="btn btn-primary" to='/administrador/crear-producto'>
+          <Link className="btn btn-primary" to="/administrador/crear-producto">
             Agregar
           </Link>
         </div>
@@ -24,12 +24,13 @@ const Administrador = () => {
               <th>Opciones</th>
             </tr>
           </thead>
-          <tbody>  
-            <ItemReceta></ItemReceta>         
+          <tbody>
+            <ItemReceta></ItemReceta>
           </tbody>
         </Table>
       </section>
-    );
+    </div>
+  );
 };
 
 export default Administrador;

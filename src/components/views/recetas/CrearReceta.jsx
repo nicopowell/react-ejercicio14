68f4b70f74342b1ef6a75427b80ba9394738ca2e
Nicolas Powell <nicopowell30@gmail.com>
2023-06-18 22:53:32 -0300

    agregué las validaciones del nombre y los ingredientes

--- src/components/views/recetas/CrearReceta.jsx
+++ src/components/views/recetas/CrearReceta.jsx
@@ -10,44 +10,77 @@ const CrearReceta = () => {
   } = useForm();
 
   const onSubmit = (recetaNueva) => {
-    consultaAgregarReceta(recetaNueva).then((respuestaCreated)=>{
-      console.log(respuestaCreated)
-      if(respuestaCreated && respuestaCreated.status === 201){
-        Swal.fire('Receta creada', `El producto ${recetaNueva.nombre} fue creado correctamente`, 'success');
+    consultaAgregarReceta(recetaNueva).then((respuestaCreated) => {
+      console.log(respuestaCreated);
+      if (respuestaCreated && respuestaCreated.status === 201) {
+        Swal.fire(
+          "Receta creada",
+          `El producto ${recetaNueva.nombre} fue creado correctamente`,
+          "success"
+        );
         reset();
-      }else{
-        Swal.fire('Ocurrió un error', `El producto ${recetaNueva.nombre} no fue creado, intentelo mas tarde`, 'error');
+      } else {
+        Swal.fire(
+          "Ocurrió un error",
+          `El producto ${recetaNueva.nombre} no fue creado, intentelo mas tarde`,
+          "error"
+        );
       }
-    })
-   
+    });
   };
 
   return (
     <section className="container py-5 mainSection">
       <h1 className="display-4">Nueva receta</h1>
       <hr />
-      <Form>
-        <Form.Group className="mb-3" controlId="formNombreProdcuto">
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className="mb-3" controlId="formNombre">
           <Form.Label>Receta*</Form.Label>
-          <Form.Control type="text" placeholder="Ej: Pizza" />
+          <Form.Control
+            type="text"
+            placeholder="Ej: Pizza"
+            {...register("nombre", {
+              required: "El nombre de la receta es obligatorio",
+              minLength: {
+                value: 2,
+                message: "La cantidad minima de caracteres es de 2",
+              },
+              maxLength: {
+                value: 100,
+                message: "La cantidad minima de caracteres es de 50",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">{errors.nombre?.message}</Form.Text>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formPrecio">
+        <Form.Group className="mb-3" controlId="formIngredientes">
           <Form.Label>Ingredientes*</Form.Label>
-          <Form.Control as="textarea" placeholder={`500g de harina 000\n700ml de agua`} />
+          <Form.Control
+            as="textarea"
+            placeholder={`Agregar ingredientes separados por ///, por ejemplo: "1kg de Harina///2 Huevos"`}
+            {...register("ingredientes", {
+              required: "Los ingredientes son un campo obligatorio",
+              pattern: {
+                value: /^[\w]{10,500}(\/\/\/\w+)?$/,
+                message: "Los ingredientes deben contener entre 10 y 500 caracteres y estar separados por /// entre ellos"
+              }
+            })}
+          />
+          <Form.Text className="text-danger">{errors?.ingredientes?.message}</Form.Text>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formPrecio">
+        <Form.Group className="mb-3" controlId="formInstrucciones">
           <Form.Label>Instrucciones*</Form.Label>
           <Form.Control as="textarea" placeholder={`Mezclar en un bol el harina con...`} />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formImagen">
+        <Form.Group className="mb-3" controlId="formTiempo">
           <Form.Label>Tiempo</Form.Label>
           <Form.Control type="text" placeholder="Ej: 40 min" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formImagen">
+        <Form.Group className="mb-3" controlId="formCantidad">
           <Form.Label>Cantidad</Form.Label>
           <Form.Control type="text" placeholder="Ej: 4 personas" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formPrecio">
+        <Form.Group className="mb-3" controlId="formCategoria">
           <Form.Label>Categoria*</Form.Label>
           <Form.Select>
             <option value="">Seleccione una opcion</option>
@@ -55,7 +88,7 @@ const CrearReceta = () => {
             <option value="Salado">Salado</option>
           </Form.Select>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formPrecio">
+        <Form.Group className="mb-3" controlId="formDescripcion">
           <Form.Label>Descripcion*</Form.Label>
           <Form.Control as="textarea" placeholder={`Esta receta de pizza facil y rapida`} />
         </Form.Group>
